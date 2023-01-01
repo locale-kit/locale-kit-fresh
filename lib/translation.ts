4d614342d0204edbc4edbba561293f41c5d6b14a
Jake Lees <brocococonut@gmail.com>
2023-01-01 23:44:25 +1100

    Bump locale-kit version

--- lib/translation.ts
+++ lib/translation.ts
@@ -1,3 +1,3 @@
-import { LocaleKit } from 'https://deno.land/x/localekit@0.4.1/mod.ts';
+import { LocaleKit } from 'https://deno.land/x/localekit@0.5.0/mod.ts';
 
 export const lang_svc = new LocaleKit();
